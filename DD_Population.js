87c6089e542b62d6a96689f321270e1abbc86fcc
Rick R <rick.ram6@gmail.com>
2019-05-20 21:17:39 -0700

    Can't get external JS file linked...

---
+++ DD_Population.js
@@ -0,0 +1,28 @@
+let stateCounty = {
+    Alabama:["Autauga","Baldwin","Barbour","Bibb","Blount","Bullock","Butler","Calhoun","Chambers","Cherokee","Choctaw"],
+    Alaska:["Aleutians","Anchorage","Bristol Bay","Denali","Fairbanks North Star","Haines","Juneau","Kenai Peninsula","Ketchikan Gateway","Kodiak Island","Lake and Peninsula"],
+    Arizona:["Apache","Cochise","Coconino","Gila","Graham","Greenlee","La Paz","Maricopa","Mohave","Navajo","Pima"],
+    Arkansas:["Arkansas","Ashley","Baxter","Benton","Boone","Bradley","Calhoun","Carroll","Chicot","Clark","Clay"],
+    California:["Alameda","Alpine","Amador","Butte","Calaveras","Colusa","Contra Costa","Del Norte","El Dorado","Fresno","Glenn"]
+    }
+
+    function getStateKey(){
+        let stateDD = document.getElementById("selState");
+        let stateLst = Object.keys(stateCounty);
+        let stateOpt = "<option value = '0'> --Select- </option>"
+        for (let i = 0; i < stateLst.length; i++){
+            stateOpt += "<option value = '" + stateLst[i] + "'>" + stateLst[i] + "</option>";
+        }
+            stateDD.innerHTML = stateOpt;                    
+    }
+
+    function getCounties(){                
+        let selectedState = document.getElementById("selState").value;
+        let countyDD = document.getElementById("selCounty");
+        let countyOpt = "<option value = '0'> --Select-- </option>"
+        for(let i = 0; i < stateCounty[selectedState].length; i++){
+             countyOpt += "<option value = '" + stateCounty[selectedState][i] + "'>" + stateCounty[selectedState][i] + "</option>";
+         }
+         countyDD.innerHTML = countyOpt;
+         countyDD.style.visibility = "visible";
+    }
