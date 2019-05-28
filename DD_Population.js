4a58ad89abd521a98aaa73bfa328b18cec16308b
Rick R <rick.ram6@gmail.com>
2019-05-27 17:14:06 -0700

    attempted to create hamburger menu but derailed by loginpage...

--- DD_Population.js
+++ DD_Population.js
@@ -26,13 +26,3 @@ let stateCounty = {
          countyDD.innerHTML = countyOpt;
          countyDD.style.visibility = "visible";
     }
-
-    function openSlideMenu(){
-        document.getElementById('side-menu').style.width = '250px';
-        document.getElementById('main').style.marginLeft = '0px';
-    }
-
-    function closeSlideMenu(){
-        document.getElementById('side-menu').style.width = '0px';
-        document.getElementById('main').style.marginLeft = '0px';
-    }
